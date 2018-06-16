4be573e7411e85dd73db81cd7f906d9e70d023dd
FLEB <fleb@flebpuzzles.com>
2018-06-16 19:50:15 -0400

    initial laravel app

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>About</title>
+	</head>
+	<body>
+		<h1>About</h1>
+		<p>This will eventually be a really cool web application. Currently it's kind of lame.</p>
+	</body>
+</html>
